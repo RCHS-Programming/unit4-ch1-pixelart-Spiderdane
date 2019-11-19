d2aa68e5bb803af7639d7a6142f557b7d43e5400
Spiderdane <dane.woodring@stu.boone.kyschools.us>
2019-11-19 12:30:55 -0500

    i done it

---
+++ resources/script.js
@@ -0,0 +1,13 @@
+function setPixelColor(pixel)
+{
+    pixel.style.backgroundColor = 'black';
+}
+var penColor = 'black';
+function setPenColor(pen)
+{
+    penColor = pen;
+}
+function setPixelColor(pixel)
+{
+    pixel.style.backgroundColor = penColor;
+}
